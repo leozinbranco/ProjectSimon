73a805c0f81cecd0e8e2e0e96de21c6188eddaed
Matheus <theus-7@hotmail.com>
2020-07-13 15:36:49 -0300

    added props to apadrinhar page

--- frontend/src/routes/tabs.routes.js
+++ frontend/src/routes/tabs.routes.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Home from '../pages/Home';
-import Apadrinhar from '../pages/Apadrinhar';
+import Apadrinhar from '../pages/List';
 import Reportar from '../pages/Reportar';
 
 const TabBottom = createBottomTabNavigator();
@@ -14,7 +14,7 @@ const TabNavigator = () => (
     inactiveTintColor: 'white',
   }}>
     <TabBottom.Screen name="Home" component={Home} />
-    <TabBottom.Screen name="Apadrinhar" component={Apadrinhar} />
+    <TabBottom.Screen name="Apadrinhar" children={()=><Apadrinhar sexo={'sexoi'}/>} />
     <TabBottom.Screen name="Reportar" component={Reportar} />
   </TabBottom.Navigator>
 );
