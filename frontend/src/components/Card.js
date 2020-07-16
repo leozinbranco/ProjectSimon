f0bf9e4c71661c0a34d25849d236284e6f4243a9
Leonardo <cyndrasilgames@gmail.com>
2020-07-16 14:56:58 -0300

    Update Card.js

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -21,11 +21,12 @@ export default function Card({ data }) {
                 source={require('../../assets/gato.png')}
             />
 
-            <Text style={Style.bottomText}>
-            <Image source={require('../../assets/ong.png')} style={Style.image}></Image>
-            ONG • {data.ong} 
-            </Text >
-
+            
+                <Text style={Style.bottomText}>
+                <Image source={require('../../assets/ong.png')} style={Style.image}></Image>
+                ONG • {data.ong} 
+                </Text >
+           
 
         </TouchableOpacity>
     )
@@ -34,23 +35,32 @@ export default function Card({ data }) {
 
 const Style = StyleSheet.create({
     card: {
-        flex: 1,
+        flex: 1, // <- ocupa o máximo de tamanho possível do elemento pai
         flexDirection: "column",
         //backgroundColor: '#3FB55D',
         margin: 10,
     },
     title: {
         fontSize: 30,
-        fontWeight: "bold"
+        fontWeight: "bold",
 
     },
     bottomText: {
         color : '#606060',
         opacity: 0.8,
+        backgroundColor: 'purple',
+        textAlign: "left",
+        textAlignVertical: "center"
+
+    },
+    image: { 
+        justifyContent: 'center',
+        resizeMode: 'contain', 
+        width: 21,
+        height: 24,
         
     },
-    image: {
-        width: 50,
-        height: 50,
-    }
+
+    
+    
 });
