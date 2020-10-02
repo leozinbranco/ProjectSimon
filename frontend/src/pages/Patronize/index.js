790711407782fe68bfe4e59b8c402a40f3c7fae9
Matheus <theus-7@hotmail.com>
2020-10-02 11:48:20 -0300

    patronize page development

--- frontend/src/pages/Patronize/index.js
+++ frontend/src/pages/Patronize/index.js
@@ -13,10 +13,14 @@ import {
 
 import { FontAwesome, FontAwesome5 } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import { Picker } from '@react-native-community/picker';
+
 
 export default function PetProfile({ route, navigation }) {
     const { animal } = route.params;
 
+    const [valor, setValor] = useState('');
+    const [paymentMethod, setPaymentMethod] = useState('');
 
     return (
 
@@ -26,12 +30,31 @@ export default function PetProfile({ route, navigation }) {
                 <Text> Pagina de apadrinhar </Text>
 
                 <Text> {`Apadrinhar ${animal.name} e ajudar a ${animal.company_name} com `} </Text>
-                <Text> R$10,00 </Text>
-                <Text> R$20,00 </Text>
-                <Text> R$30,00 </Text>
+
+
+                <Picker
+                    selectedValue={valor}
+                    style={{ height: 50, width: '90%' }}
+                    onValueChange={(itemValue, itemIndex) =>
+                        setValor(itemValue)
+                    }>
+                    <Picker.Item label="R$10,00" value={10} />
+                    <Picker.Item label="R$20,00" value={20} />
+                    <Picker.Item label="R$30,00" value={30} />
+                </Picker>
+
 
                 <View>
                     <Text> Escolha um meio de pagamento</Text>
+
+                    <Picker
+                        selectedValue={paymentMethod}
+                        style={{ height: 50, width: '90%' }}
+                        onValueChange={(itemValue, itemIndex) =>
+                            setPaymentMethod(itemValue)
+                        }>
+                        <Picker.Item label="Cartão de credito" value={10} />
+                    </Picker>
 
                 </View>
 
