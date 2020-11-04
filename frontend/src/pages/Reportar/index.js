cc55660b49c6c1cd4aafeacb2c93a5229692c09b
Leonardo <cyndrasilgames@gmail.com>
2020-11-04 15:11:21 -0300

    Added carousel, markers, sincronized with the reports. Need to solve some issues in Android(onCarouselItemChange)

--- frontend/src/pages/Reportar/index.js
+++ frontend/src/pages/Reportar/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
     SafeAreaView,
     StyleSheet,
@@ -9,9 +9,23 @@ import {
     Dimensions,
     ActivityIndicator,
     TouchableOpacity,
+    Image,
 } from 'react-native';
+import {
+    Paragraph,
+    Caption,
+    Divider,
+    Avatar,
+    Button,
+    Card,
+    IconButton,
+    Title,
+    Colors,
+    Switch,
+} from 'react-native-paper';
 import Style from './style'
 import MapView, { PROVIDER_GOOGLE, Marker, Callout, Circle } from 'react-native-maps';
+import Carousel from 'react-native-snap-carousel'
 //import {Location, Permissions } from 'expo';
 import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
@@ -30,13 +44,10 @@ export default function Reportar({ navigation }) {
     const [loading, setLoading] = useState(true);
     const [reports, setReports] = useState([]);
 
-    const arrayPolygon = [
-        { name: '1', latitude: 37.382935, longitude: -122.412355 },
-        { name: '2', latitude: 37.782935, longitude: -122.422355 },
-        { name: '3', latitude: 37.772935, longitude: -122.412355 },
-        { name: '4', latitude: 37.792935, longitude: -122.457655 },
-        { name: '5', latitude: 37.742935, longitude: -122.459655 },
-    ]
+    const mapRef = useRef(null);
+    const markerRef = useRef(null);
+    const carouselRef = useRef(null);
+
     //const [coords, setCoords] = useState({});
     //const [hasLocationPermissions, setHasLocationPermissions] = useState(false);
     //const [locationResult, setLocationResult] = useState({});
@@ -60,8 +71,6 @@ export default function Reportar({ navigation }) {
                 {
                     headers: { Authorization: `Bearer ${api_token}` }
                 }).then((response) => {
-
-                    //alert(JSON.stringify(response.data));
                     setReports(response.data);
 
                 }).catch((error) => {
@@ -78,97 +87,148 @@ export default function Reportar({ navigation }) {
         _getReportsLocation();
         let gpsServiceStatus = await Location.hasServicesEnabledAsync();
 
-
         let { status } = await Permissions.askAsync(Permissions.LOCATION);
-        //alert(status);
-
         if (status === 'granted' && gpsServiceStatus) {
             const location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
-            //alert(JSON.stringify(location.coords));
-            //setLatitude(location.coords.latitude);
-            ///setLongitude(location.coords.longitude);
             setRegion({
                 latitude: location.coords.latitude,
                 longitude: location.coords.longitude,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421
             });
+
+
             setLoading(false);
-            /*setCoords({ 
-                latitude: location.coords.latitude, 
-                longitude: location.coords.longitude
-            })*/
-            //alert(JSON.stringify(region));
-            //alert(latitude);
-            //alert(longitude);
-            //alert(coords);
-            /*setLocationResult({
-                locationResult: 'Permission to access location was denied',
-            });*/
         } else {
-            //setHasLocationPermissions(true);
+
             alert("Ocorreu um erro ! Autorize o aplicativo para acessar a localização!");
         }
-        //setMapRegion({latitude: locationResult.coords.latitude, longitude: locationResult.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421});
 
     };
 
+    onCarouselItemChange = (index) => {
+
+        mapRef.current.animateToRegion({
+            latitude: reports[index].map_lati - 0.01,
+            longitude: reports[index].map_long,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+        })
+
+        reports[index].current.showCallout();
+    }
+
+    onMarkerPressed = (index) => {
+        mapRef.current.animateToRegion({
+            latitude: reports[index].map_lati - 0.01,
+            longitude: reports[index].map_long,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+        });
+
+        carouselRef.current.snapToItem(index);
+    }
+
+    const renderCarouselItem = ({ item }) => {
+        return (
+            <View style={styles.cardContainer} key={`id_report_${item.id}`}>
+                <Title>{item.title}</Title>
+                <Caption>{item.desc}</Caption>
+                <Divider />
+
+                <Image source={require('../../../assets/gato.png')} style={styles.imageCarousel} />
+
+                <Button
+                    onPress={() => alert("apertou")}
+                    mode="contained"
+                    style={styles.buttonCarousel}
+
+                >
+                    Contatar
+                </Button>
+
+            </View>
+        );
+    }
+
     return (
         <SafeAreaView style={styles.screen}>
-            <View style={styles.container}>
-
-                {
-                    loading ? (<ActivityIndicator size="large" animating={loading} color={'gray'} />)
-                        :
-                        (<MapView style={styles.mapStyle} provider={PROVIDER_GOOGLE} region={region}
-                            initialRegion={{
-                                latitude: 37.78825,
-                                longitude: -122.4324,
-                                latitudeDelta: 0.0922,
-                                longitudeDelta: 0.0421,
-                            }}
-                            showsUserLocation={true}
-                            showsMyLocationButton={true}
-
-                        >
 
 
+            {
+                loading ? (<ActivityIndicator size="large" animating={loading} color={'gray'} />)
+                    :
+                    (
+                        <View style={styles.container}>
+                            <MapView style={styles.mapStyle} provider={PROVIDER_GOOGLE} region={region}
+                                ref={mapRef}
+                                initialRegion={{
+                                    latitude: 37.78825,
+                                    longitude: -122.4324,
+                                    latitudeDelta: 0.0922,
+                                    longitudeDelta: 0.0421,
+                                }}
+                                showsUserLocation={true}
+                                showsMyLocationButton={true}
 
-
-                            {
-                                reports.map(report => {
-                                    //alert(JSON.stringify(report));
-                                    return (
-                                        <>
-                                            <Circle
-                                                center={{ latitude: Number(report.map_lati), longitude: Number(report.map_long) }}
-                                                radius={500}
-                                                fillColor="rgba(193,66,66,0.5)"
-                                                key={"id_report_" + report.id}
-                                                strokeWidth={0}
-                                            ></Circle>
-                                            <Marker
-                                                coordinate={{ latitude: Number(report.map_lati), longitude: Number(report.map_long) }}
-                                                image={require('../../../assets/rescue.png')}
-                                                title={report.title}
-                                            >
-                                            
-                                            </Marker>
-
-
-                                        </>
+                            >
 
 
 
-                                    );
-                                })
-                            }
+
+                                {
+                                    reports.map((report, index) => {
+                                        //alert(JSON.stringify(report));
+                                        return (
+                                            <>
+                                                <Circle
+                                                    center={{ latitude: Number(report.map_lati), longitude: Number(report.map_long) }}
+                                                    radius={500}
+                                                    fillColor="rgba(193,66,66,0.5)"
+                                                    key={"id_report_" + report.id}
+                                                    strokeWidth={0}
+                                                ></Circle>
+                                                <Marker
+                                                    ref={ref => {reports[index] = ref; } }
+                                                    coordinate={{ latitude: Number(report.map_lati), longitude: Number(report.map_long) }}
+                                                    image={require('../../../assets/rescue.png')}
+                                                    title={report.title}
+                                                    onPress={() => onMarkerPressed(index)}
+                                                    
+                                                >
+                                                    <Callout>
+                                                        <Text>An Interesting city</Text>
+                                                    </Callout>
+                                                </Marker>
+
+
+                                            </>
 
 
 
-                        </MapView>)
-                }
-            </View>
+                                        );
+                                    })
+                                }
+
+
+
+                            </MapView>
+                            <Carousel
+                                ref={carouselRef}
+                                data={reports}
+                                renderItem={renderCarouselItem}
+                                sliderWidth={Dimensions.get('window').width}
+                                itemWidth={300}
+                                containerCustomStyle={styles.carousel}
+                                onSnapToItem={(index) => onCarouselItemChange(index)}
+                                removeClippedSubviews={false}
+                            />
+                        </View>
+                    )
+            }
+
+
+
         </SafeAreaView>
     );
 
@@ -180,6 +240,9 @@ const styles = StyleSheet.create({
     screen: {
         width: Dimensions.get('window').width,
         height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
     },
 
     mapStyle: {
@@ -192,7 +255,41 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         alignSelf: 'center',
-
     },
+    carousel: {
+        position: 'absolute',
+        bottom: 0,
+        marginBottom: 40,
+    },
+    cardContainer: {
+        backgroundColor: 'white',
+        height: 270,
+        width: 300,
+        padding: 18,
+        borderRadius: 24
+    },
+    cardTitle: {
+        color: 'white',
+        fontSize: 22,
+        alignSelf: 'center'
+    },
+    buttonCarousel: {
+        position: 'absolute',
+        bottom: 15,
+        backgroundColor: 'red',
+        height: 30,
+        width: 250,
+        alignSelf: 'center',
+        justifyContent: 'center',
+    },
+    imageCarousel: {
+        alignSelf: 'center',
+        justifyContent: 'center',
+        margin: 0,
+        borderRadius: 15,
+        height: 100,
+        width: '50%',
+    },
+
 });
 
