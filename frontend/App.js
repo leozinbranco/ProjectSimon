4f196009f6b5c4252b6d653279b6d30d8f5676d3
Leonardo <cyndrasilgames@gmail.com>
2020-12-07 18:16:26 -0300

    report page almost done

--- frontend/App.js
+++ frontend/App.js
@@ -2,12 +2,15 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import Routes from './src/routes';
 import { AuthProvider } from './src/services/auth'
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
+    <PaperProvider>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </PaperProvider>
   );
 };
 
